7b2d84460e3906be9248185ecdfa7c5343762bb0
Chukwuebuka Onah <revelationjay02@gmail.com>
2023-11-26 07:21:38 +0100

    Added app Expiration

--- lib/app.dart
+++ lib/app.dart
@@ -5,13 +5,37 @@ import 'package:plant_disease_detection/ui/theme/app_theme.dart';
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  // App Expires 15th december
+  bool get isExpired => DateTime.now().isAfter(DateTime(2023, 12, 15));
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: OnboardingPage(),
+      home: isExpired ? ErrorPage() : OnboardingPage(),
       theme: appTheme,
       themeMode: ThemeMode.light,
       debugShowCheckedModeBanner: false,
     );
   }
 }
+
+class ErrorPage extends StatelessWidget {
+  const ErrorPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text("404", style: TextStyle(fontSize: 100, fontWeight: FontWeight.bold) ),
+            const SizedBox(height: 30),
+            Text("An Unknown error occured"),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
